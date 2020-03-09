a6404a197a6217b34d8eada0c4308dfcfc73f2ba
Maxime Désécot <maxime.desecot@gmail.com>
2020-03-09 16:26:48 +0100

    fix(DocumentFolder): raise if extract uuid failed

--- lib/zm/client/folder/mod_document_folder.rb
+++ lib/zm/client/folder/mod_document_folder.rb
@@ -2,10 +2,13 @@ module DocumentFolder
   UUID_REGEX = %r{[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}:[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}}
 
   def upload(file_path)
-    puts "Document folder: #{file_path}"
     uploader = Zm::Client::Upload.new(@parent, Zm::Client::RestAccountConnector.new)
     str = uploader.upload_attachment(file_path)
+
     uuid = str.scan(UUID_REGEX).first
+
+    raise Zm::Client::RestError, "Upload failed" if uuid.nil?
+
     upload_options = { upload: { id: uuid } }
     @parent.sacc.save_document(@parent.token, id, upload_options)
   end
