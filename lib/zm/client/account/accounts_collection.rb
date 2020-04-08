c08184ee308d25a68434d2f1608750779cd465fe
Maxime Désécot <maxime.desecot@gmail.com>
2020-04-08 12:03:33 +0200

    fix(SoapAdminConnector): add reset_query_params after find_by request

--- lib/zm/client/account/accounts_collection.rb
+++ lib/zm/client/account/accounts_collection.rb
@@ -17,6 +17,7 @@ module Zm
 
       def find_by(hash)
         rep = sac.get_account(hash.values.first, hash.keys.first, attrs_comma, @apply_cos)
+        reset_query_params
         entry = rep[:Body][:GetAccountResponse][:account].first
         # puts "AccountsCollection find_by #{@parent.class} #{@parent.object_id} #{@parent.soap_admin_connector}"
         account = Account.new(@parent)
