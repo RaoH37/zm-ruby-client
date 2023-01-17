2c09091dc2cf24fb78b3ca01ccca3e247096f57a
Maxime Désécot <maxime.desecot@gmail.com>
2023-01-17 19:52:34 +0100

    comment: remove code commented

--- lib/zm/client/domain/domain.rb
+++ lib/zm/client/domain/domain.rb
@@ -4,26 +4,10 @@ module Zm
   module Client
     # objectClass: zimbraDomain
     class Domain < Base::AdminObject
-      INSTANCE_VARIABLE_KEYS = %i[name description zimbraDomainName zimbraDomainStatus zimbraId zimbraDomainType
-        zimbraDomainDefaultCOSId zimbraGalAccountId zimbraPreAuthKey zimbraGalLdapBindDn zimbraGalLdapBindPassword
-        zimbraGalLdapFilter zimbraGalLdapSearchBase zimbraGalLdapURL zimbraGalMode zimbraMailTransport]
-
-      attr_accessor *INSTANCE_VARIABLE_KEYS
-
       def initialize(parent)
         super(parent)
         @grantee_type = 'dom'.freeze
       end
-
-      # def to_h
-      #   hashmap = Hash[all_instance_variable_keys.map { |key| [key, instance_variable_get(arrow_name(key))] }]
-      #   hashmap.delete_if { |_, v| v.nil? }
-      #   hashmap
-      # end
-
-      # def all_instance_variable_keys
-      #   INSTANCE_VARIABLE_KEYS
-      # end
 
       def create!
         rep = sac.create_domain(jsns_builder.to_jsns)
@@ -60,38 +44,6 @@ module Zm
       def accounts
         @accounts ||= DomainAccountsCollection.new(self)
       end
-
-      # def account_quotas(server_id)
-      #   rep = sac.get_quota_usage(name, nil, nil, nil, nil, nil, nil, server_id)
-      #   AccountsBuilder.new(@parent, rep).make
-      # end
-
-      # def init_from_json(json)
-      #   super(json)
-      #   return unless json[:a].is_a? Array
-      #
-      #   n_key = :n
-      #   c_key = :_content
-      #   at_key = '@'
-      #
-      #   attrs = {}
-      #
-      #   # TODO: definir ici le typage fort des attributs pour ne pas avoir
-      #   # a faire des cases sur les class des attributs dans le code.
-      #
-      #   json[:a].each do |a|
-      #     k = "#{at_key}#{a[n_key]}".to_sym
-      #     v = a[c_key]
-      #     if !attrs[k].nil?
-      #       attrs[k] = [attrs[k]] unless attrs[k].is_a?(Array)
-      #       attrs[k].push(v)
-      #     else
-      #       attrs[k] = v
-      #     end
-      #   end
-      #
-      #   attrs.each { |k, v| instance_variable_set(k, v) }
-      # end
 
       def attrs_write
         @parent.zimbra_attributes.all_domain_attrs_writable_names
