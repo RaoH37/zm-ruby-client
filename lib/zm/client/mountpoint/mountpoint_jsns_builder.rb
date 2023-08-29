8dd5697d90e818a8bba8a642d3f9b6d7596e1667
Maxime Désécot <maxime.desecot@gmail.com>
2023-08-29 17:45:35 +0200

    fix(MountPoint): missing setters

--- lib/zm/client/mountpoint/mountpoint_jsns_builder.rb
+++ lib/zm/client/mountpoint/mountpoint_jsns_builder.rb
@@ -17,7 +17,6 @@ module Zm
           color: @item.color,
           rgb: @item.rgb,
           url: @item.url,
-          fb: @item.fb,
           view: @item.view,
           zid: @item.zid,
           rid: @item.rid
@@ -38,7 +37,6 @@ module Zm
           color: @item.color,
           rgb: @item.rgb,
           url: @item.url,
-          fb: @item.fb,
           view: @item.view
         }.delete_if { |_, v| v.nil? }
 
