a53e6adca460f54ee89e3ad307966b7b28ed6714
Maxime Désécot <maxime.desecot@gmail.com>
2023-11-16 15:18:22 +0100

    refactor(contact): split id if contact from a shared folder

--- lib/zm/client/contact/contact_member.rb
+++ lib/zm/client/contact/contact_member.rb
@@ -3,26 +3,40 @@
 module Zm
   module Client
     class ConcatMember
+      include Zm::Inspector
+
       INTERNAL = 'C'
       FREE     = 'I'
       LDAP     = 'G'
       ADD      = '+'
       DEL      = '-'
 
-      attr_reader :type, :value, :op
+      attr_reader :type, :value, :op, :shared_account_id
 
       def initialize(type, value, op = nil)
+        @shared_account_id = nil
         @op = op
         @type = type
-        @value = value
+
+        value_int = value.to_i
+
+        if !value_int.zero?
+          @value = value_int
+        elsif Zm::Client::Regex::SHARED_CONTACT.match(value)
+          part_value = value.split(':')
+          @shared_account_id = part_value.first
+          @value = part_value.last.to_i
+        else
+          @value = value
+        end
       end
 
       def internal?
-        @type == INTERNAL && !Zm::Client::Regex::SHARED_CONTACT.match(value)
+        @type == INTERNAL && @value.is_a?(Integer)
       end
 
       def shared?
-        @type == INTERNAL && Zm::Client::Regex::SHARED_CONTACT.match(value)
+        @type == INTERNAL && !@shared_account_id.nil?
       end
 
       def free?
@@ -43,6 +57,17 @@ module Zm
 
       def remove!
         @op = DEL
+      end
+
+      def email_address
+        return unless ldap?
+
+        @email_address ||= @value.sub('uid=', '').sub(',ou=people,dc=', '@').gsub(',dc=', '.')
+      end
+
+      def instance_variables_map
+        email_address
+        super
       end
     end
   end
