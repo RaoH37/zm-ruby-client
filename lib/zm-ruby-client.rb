fbf86365f54dad978c37d7e4121e08ac3222ddc1
Maxime Désécot <maxime.desecot@gmail.com>
2024-10-24 18:48:30 +0200

    feat: add console executable to the project

--- lib/zm-ruby-client.rb
+++ lib/zm-ruby-client.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
 require 'zm/client'
+
+Kernel.warn "⚠️ [DEPRECATION] memorization will not be supported in zm-ruby-client 3.0. Please do not use .all method as memorized items."
