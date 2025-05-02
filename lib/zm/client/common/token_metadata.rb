ae1715b19bb70bf5fe5bc6a121e555c4ee1b4e62
Maxime Désécot <maxime.desecot@gmail.com>
2025-05-02 19:56:11 +0200

    rebuild release 2.2.6

---
+++ lib/zm/client/common/token_metadata.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Zm
+  module Client
+    class TokenMetaDataError < StandardError; end
+
+    class TokenMetaData
+      def initialize(token)
+        raise TokenMetaDataError, 'no such token' unless token.is_a?(String)
+
+        parts = token.split('_')
+        raise TokenMetaDataError, 'invalid token' unless parts.length == 3
+
+        @key_id = parts[0]
+        @hmac = parts[1]
+        @encoded = parts[2]
+      end
+
+      def decoded
+        @decoded ||= [@encoded].pack('H*')
+      end
+
+      def metadatas
+        @metadatas ||= Hash[decoded.split(/;/).map { |part| part.split(/=\d+:/) }].freeze
+      end
+
+      def zimbra_id
+        @zimbra_id ||= metadatas['id']
+      end
+
+      def is_admin?
+        @is_admin ||= metadatas['admin'] == '1'
+      end
+
+      def server_version
+        metadatas['version']
+      end
+
+      def expire_at
+        @expire_at ||= Time.at(metadatas['exp'].to_f / 1000).freeze
+      end
+    end
+  end
+end
