fb1b8001ec44ca7ba83ae8c3111482e31f2d2876
Maxime Désécot <maxime.desecot@gmail.com>
2026-06-22 12:57:09 +0200

    refactor: remove deprecated warning

--- lib/zm-ruby-client.rb
+++ lib/zm-ruby-client.rb
@@ -1,6 +1,3 @@
 # frozen_string_literal: true
 
 require 'zm/client'
-
-Kernel.warn '⚠️ [DEPRECATION] memorization will not be supported in zm-ruby-client 3.0. Please do not use .all method as memorized items.'
-Kernel.warn '⚠️ [DEPRECATION] zm-ruby-client 3.0 will not support Ruby < 3.1'
